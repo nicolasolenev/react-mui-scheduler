8fa75897d588fd0d6e5c25809d8d500f369d9be0
nicolasolenev <cardinal2113@gmail.com>
2023-09-14 15:33:15 +0300

    add ru locale

--- dist/locales/i18n.js
+++ dist/locales/i18n.js
@@ -6,24 +6,26 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const i18next_1 = require("i18next");
 const react_i18next_1 = require("react-i18next");
 const common_json_1 = __importDefault(require("./en/common.json"));
-const common_json_2 = __importDefault(require("./fr/common.json"));
-const common_json_3 = __importDefault(require("./ko/common.json"));
-const common_json_4 = __importDefault(require("./de/common.json"));
-const common_json_5 = __importDefault(require("./es/common.json"));
-const common_json_6 = __importDefault(require("./ar/common.json"));
-const common_json_7 = __importDefault(require("./ja/common.json"));
-const common_json_8 = __importDefault(require("./zh/common.json"));
-const common_json_9 = __importDefault(require("./pt-br/common.json"));
+const common_json_2 = __importDefault(require("./ru/common.json"));
+const common_json_3 = __importDefault(require("./fr/common.json"));
+const common_json_4 = __importDefault(require("./ko/common.json"));
+const common_json_5 = __importDefault(require("./de/common.json"));
+const common_json_6 = __importDefault(require("./es/common.json"));
+const common_json_7 = __importDefault(require("./ar/common.json"));
+const common_json_8 = __importDefault(require("./ja/common.json"));
+const common_json_9 = __importDefault(require("./zh/common.json"));
+const common_json_10 = __importDefault(require("./pt-br/common.json"));
 const resources = {
     en: { common: common_json_1.default },
-    fr: { common: common_json_2.default },
-    ko: { common: common_json_3.default },
-    de: { common: common_json_4.default },
-    es: { common: common_json_5.default },
-    ar: { common: common_json_6.default },
-    ja: { common: common_json_7.default },
-    zh: { common: common_json_8.default },
-    br: { common: common_json_9.default },
+    ru: { common: common_json_2.default },
+    fr: { common: common_json_3.default },
+    ko: { common: common_json_4.default },
+    de: { common: common_json_5.default },
+    es: { common: common_json_6.default },
+    ar: { common: common_json_7.default },
+    ja: { common: common_json_8.default },
+    zh: { common: common_json_9.default },
+    br: { common: common_json_10.default },
 };
 const i18n = (0, i18next_1.createInstance)({
     resources,
